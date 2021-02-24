462a66c6a5bf2c01db81315474c7c50f8dfa4147
James Gould <nobleburgundy@gmail.com>
2021-02-23 18:38:04 -0600

    Minor update to mongoose.connect for heroku

--- server.js
+++ server.js
@@ -15,15 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false,
-// });
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  // useCreateIndex: true,
+  useCreateIndex: true,
   useFindAndModify: false,
 });
 
